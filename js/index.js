a24b7b458a27aac844d8b570495b0b2318ee39a9
InterlockGo <cord3576@bears.unco.edu>
2023-02-25 23:05:50 -0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -9,3 +9,11 @@ const checkScreenWidth = () => {
 checkScreenWidth();
 window.addEventListener('resize', checkScreenWidth);
 
+const links = document.querySelectorAll('nav a');
+links.forEach(link => {
+  link.addEventListener('click', event => {
+    event.preventDefault();
+    const target = document.querySelector(link.getAttribute('href'));
+    target.scrollIntoView({ behavior: 'smooth' });
+  });
+});
