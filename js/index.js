cf548380ff10cb3f7729a65fc470524581323ef0
InterlockGo <cord3576@bears.unco.edu>
2023-02-25 23:01:06 -0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
+const checkScreenWidth = () => {
+  const screenWidth = window.innerWidth;
+  if (screenWidth < 600) {
+    document.querySelector('nav ul').classList.add('collapsed');
+  } else {
+    document.querySelector('nav ul').classList.remove('collapsed');
+  }
+};
+checkScreenWidth();
+window.addEventListener('resize', checkScreenWidth);
 
