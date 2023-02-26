2f3fc06dc8369bebdaeaa56bd0edc59061309b1c
InterlockGo <cord3576@bears.unco.edu>
2023-02-26 00:50:05 -0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,19 +1,21 @@
-const checkScreenWidth = () => {
-  const screenWidth = window.innerWidth;
-  if (screenWidth < 600) {
-    document.querySelector('nav ul').classList.add('collapsed');
-  } else {
-    document.querySelector('nav ul').classList.remove('collapsed');
-  }
-};
-checkScreenWidth();
-window.addEventListener('resize', checkScreenWidth);
+window.addEventListener('DOMContentLoaded', () => {
+  const checkScreenWidth = () => {
+    const screenWidth = window.innerWidth;
+    if (screenWidth < 600) {
+      document.querySelector('nav ul').classList.add('collapsed');
+    } else {
+      document.querySelector('nav ul').classList.remove('collapsed');
+    }
+  };
+  checkScreenWidth();
+  window.addEventListener('resize', checkScreenWidth);
 
-const links = document.querySelectorAll('nav a');
-links.forEach(link => {
-  link.addEventListener('click', event => {
-    event.preventDefault();
-    const target = document.querySelector(link.getAttribute('href'));
-    target.scrollIntoView({ behavior: 'smooth' });
+  const links = document.querySelectorAll('nav a');
+  links.forEach(link => {
+    link.addEventListener('click', event => {
+      event.preventDefault();
+      const target = document.querySelector(link.getAttribute('href'));
+      target.scrollIntoView({ behavior: 'smooth' });
+    });
   });
 });
